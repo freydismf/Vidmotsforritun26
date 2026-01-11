434f7f67af7242c3ef2e21211b07321da58519ae
Ebba Þóra Hvannberg <ebba@hi.is>
2026-01-11 17:04:33 +0000

    Bætti við JavaDoc lýsingu á atburðarhandlera

--- Vika1/HalloHeimurJavaFX/src/main/java/is/vidmot/Controller.java
+++ Vika1/HalloHeimurJavaFX/src/main/java/is/vidmot/Controller.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Label;
 /******************************************************************************
  *  Nafn    : Ebba Þóra Hvannberg
  *  T-póstur: ebba@hi.is
- *
  *  Lýsing  : Controller eða stýring fyrir notendaviðmótið
  *
  *
@@ -14,11 +13,13 @@ import javafx.scene.control.Label;
 public class Controller {
 
     @FXML
-    private Label welcomeText;
+    private Label welcomeText; // Viðmótshlutur sem geymir texta með kveðju
 
     @FXML
+    /**
+     * Birtir texta sem viðbragð við atburði frá hnappi
+     */
     protected void onHelloButtonClick() {
-
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
