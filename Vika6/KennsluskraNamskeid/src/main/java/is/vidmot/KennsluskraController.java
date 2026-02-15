92c5f65a9b7c724301a0d9a0c6d2081347a57fa8
Ebba Þóra Hvannberg <ebba@hi.is>
2026-02-15 16:50:11 +0000

    getSelectedIndex() notað og hnappar gerðir óvirkir ef ekert er valið

---
+++ Vika6/KennsluskraNamskeid/src/main/java/is/vidmot/KennsluskraController.java
@@ -0,0 +1,97 @@
+
+package is.vidmot;
+
+import javafx.fxml.FXMLLoader;
+import is.vinnsla.Kennsluskra;
+import is.vinnsla.Namskeid;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * Sýnir hvernig notaður er klasinn
+ * Námskeið í ListView.
+ *
+ * @author Ebba Þóra Hvannberg ebba@hi.is
+ */
+public class KennsluskraController {
+
+    // Viðmótshlutir
+    @FXML
+    private ListView<Namskeid> fxListiNamskeid;
+    @FXML
+    private Button fxSkoda;
+    @FXML
+    private Button fxEyda;
+
+    // Vinnsluhlutinn með gögnunum
+    private Kennsluskra kennsluskra = new Kennsluskra();
+    // Controller fyrir Námskeið
+    private NamskeidController namskeidController;
+
+    /**
+     * Vinnsluhluti tengdur við notendaviðmótið. Geymir núerandi index sem er valinn
+     * í listanum
+     */
+    public void initialize() {
+
+        fxListiNamskeid.setItems(kennsluskra.getOllNamskeid()); // tengja listann við viðmótið
+        // gera hnappa óvirka ef ekkert er valið
+        fxEyda.disableProperty().bind(
+                fxListiNamskeid.getSelectionModel().selectedItemProperty().isNull());
+        fxSkoda.disableProperty().bind(
+                fxListiNamskeid.getSelectionModel().selectedItemProperty().isNull());
+
+
+        try {
+            namskeidController = hladaGluggi();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Smíða notendaviðmótið fyrir dialoginn
+     *
+     * @return námskeiðs controller
+     * @throws IOException ef lesturinn gengur ekki
+     */
+    private NamskeidController hladaGluggi() throws IOException {
+        FXMLLoader dLoader = new FXMLLoader(getClass().getResource("Namskeid.fxml"));
+        dLoader.load();
+        return dLoader.getController();
+    }
+
+    /**
+     * Handler fyrir að eyða námskeiði
+     * @param ignored ónotað
+     */
+    @FXML
+    private void eydaNamskeidiHandler(ActionEvent ignored) {
+        kennsluskra.eydaNamskeidi(fxListiNamskeid.getSelectionModel().getSelectedIndex());
+    }
+
+    /**
+     * Handler fyrir skoða hnappinn. Birtir námskeið í sér glugga
+     * @param ignored ónotað
+     */
+    @FXML
+    private void skodaNamskeidHandler(ActionEvent ignored) {
+        skodaNamskeid(fxListiNamskeid.getSelectionModel().getSelectedIndex());
+    }
+
+
+    /**
+     * Birtir námskeið i í sérglugga
+     * @param i númer námskeiðs
+     */
+    private void skodaNamskeid(int i) {
+        namskeidController.birtaGluggi(
+                kennsluskra.getNamskeid(i));
+    }
+}
+
